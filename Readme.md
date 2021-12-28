142991b6278043f05a8b01c9738656adffceaf3c
Barton Chittenden <bartonski@gmail.com>
2021-12-28 14:49:34 -0500

    Initial commit of Readme.md, add CLI options

---
+++ Readme.md
@@ -0,0 +1,38 @@
+# Juggling Detector
+
+An object detection program to show trails of hands and juggling balls while
+juggling.
+
+## Usage
+
+    python3 juggling_detector.py [-s|--start_frame START] [-e|--end_frame] VIDEO FILE
+
+This will play VIDEO FILE, drawing an outline around objects detected in the current frame, as well as the trail of the center of mass of each object, taken over time.
+
+An output video file will be written out to a file that has a file name based on the video file, with `.detector` added before the file extension. For example if VIDEO FILE is `3_balls.mp4`, the output file will be `3_balls.detector.mp4`
+
+The option `-s` or `--start_frame`, with the argument of **START** can be used to specify that object detection should start on frame number **START**, likewise `-e` or `--end_frame` with the argument of **END** will stop detection at **END**.
+
+## Examples
+
+    python3 juggling_detector.py 3_balls.mp4
+
+Track and display objects in `3_balls.mp4`, write output to `3_balls.detector.mp4`
+
+
+    python3 juggling_detector.py --start_frame 100 --end_frame 1500 3_balls.mp4
+
+Track and display objects in `3_balls.mp4` between frames 100 and 1500, write output to `3_balls.detector.mp4`
+
+## Prerequisites
+
+Requires Python3 and the cv2 library. Running under Ubuntu, you should be able to run
+
+    sudo apt install python3
+    sudo apt install pip3
+    sudo apt install build-essential
+    pip3 install opencv-python
+
+A google search for 'python install cv2 windows 10', 'python install cv2 OSX', etc should give you instruction on installing the cv2 libraries.
+
+Some environements may call python 3 as `python` rather than `python3`.
---
+++ juggling_detector.py
@@ -0,0 +1,97 @@
+import cv2
+import os
+import sys
+import re
+import getopt
+argv = sys.argv[1:]
+
+start_frame = 0
+end_frame = None
+
+try:
+    opts, args = getopt.getopt(argv, "s:e:", 
+                                ["start_frame =",
+                                "end_frame ="])
+    
+except:
+    print("Error")
+
+for opt, arg in opts:
+    if opt in ['-s', '--start_frame']:
+        start_frame = arg
+    elif opt in ['-e', '--end_frame']:
+        end_frame = arg
+
+filename = str(sys.argv[-1])
+current_frame = 0
+
+cap = cv2.VideoCapture(filename)
+width      = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height     = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+if end_frame is None:
+    end_frame = frame_count
+#r = re.compile('(.*)\.([^.]+$)')
+#basename = r.sub( '', filename )
+result = re.search( '(.*)\.([^.]+$)', filename )
+basename, extension = result.groups()
+label="detector"
+output_file=f"{basename}.{label}.{extension}"
+#print( f"basename: {basename}, extension: {extension}")
+
+# Object detection from stable camera
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
+
+
+out = cv2.VideoWriter(
+        output_file,
+        cv2.VideoWriter_fourcc('M','J','P','G'),
+        frame_rate/2,
+        (width, height)
+      )
+
+print( f"size: {width}x{height}");
+
+ret, frame = cap.read()
+
+centers = []
+
+while ret and current_frame <= end_frame:
+    if current_frame >= start_frame:
+        mask = object_detector.apply(frame)
+        _, mask = cv2.threshold( mask, 254, 255, cv2.THRESH_BINARY )
+        contours, _ = cv2.findContours( mask,
+                                        cv2.RETR_TREE,
+                                        cv2.CHAIN_APPROX_SIMPLE )
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            if area > 400:
+                M = cv2.moments(cnt)
+                current = [int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])]
+                centers.append( current )
+                cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 2)
+                for center in centers:
+                    red   = 0xFF
+                    color = ( 0, 0, red )
+                    cv2.circle(frame, (center[0], center[1]), 2, color, -1)
+                cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 2)
+
+        out.write(frame)
+
+        cv2.namedWindow("Frame")        # Create a named window
+        cv2.moveWindow("Frame", 40,30)  # Move it to (40,30)
+        cv2.imshow("Frame", frame)
+        #cv2.imshow("Mask", mask)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+    ret, frame = cap.read()
+    current_frame += 1
+
+cap.release()
+out.release()
+#cv2.destroyAllWindows()
