56ef5435a4ea72197b085fbb2ff4da023d1229be
Barton Chittenden <bartonski@gmail.com>
2022-01-02 16:45:26 -0500

    Add object tracking

--- juggling_detector.py
+++ juggling_detector.py
@@ -3,6 +3,7 @@ import os
 import sys
 import re
 import getopt
+import math
 argv = sys.argv[1:]
 
 try:
@@ -86,10 +87,19 @@ if end_frame is None:
 # Object detection from stable camera
 object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
 
-print( f"size: {width}x{height}");
-print( f"start_frame: {start_frame}");
-print( f"end_frame: {end_frame}");
-print( f"trails: {trails}");
+print( f"size: {width}x{height}")
+print( f"video frame count: {frame_count}")
+print( f"start_frame: {start_frame}")
+print( f"end_frame: {end_frame}")
+print( f"selection frame count: {end_frame-start_frame}")
+print( f"trails: {trails}")
+print( f"show_mask: {show_mask}" )
+print( f"grey_threshold: {grey_threshold}" )
+print( f"area_threshold: {area_threshold}" )
+print( f"area_labels: {area_labels}" )
+print( f"grid: {grid}" )
+print( f"grid_spacing: {grid_spacing}" )
+print( f"grid_width: {grid_width}" )
 
 current_frame = 0
 centers = []
@@ -104,65 +114,118 @@ def show_grid( image ):
     for x in range(0, width, grid_spacing):
         cv2.line(image, ( x, 0), (x, height), grid_color, grid_width  )
 
-def get_center( image, contour ):
+def get_center( contour ):
                 M = cv2.moments(contour)
                 return [ int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]) ]
 
-def area_labels( image, area, label_offset, shadow_offset ):
+def object_labels( image, text, center, label_offset, shadow_offset ):
     offset=label_offset-shadow_offset
-    cv2.putText(image, f"{area}", (current[0]-offset, current[1]-offset),
+    cv2.putText(image, f"{text}", (center[0]-offset, center[1]-offset),
                 cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2 )
     offset=label_offset
-    cv2.putText(image, f"{area}", (current[0]-offset, current[1]-offset),
+    cv2.putText(image, f"{text}", (center[0]-offset, center[1]-offset),
+                cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2 )
+
+def label( image, text, center, label_offset, shadow_offset ):
+    offset=label_offset-shadow_offset
+    cv2.putText(image, f"{text}", (center[0]-offset, center[1]-offset),
+                cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2 )
+    offset=label_offset
+    cv2.putText(image, f"{text}", (center[0]-offset, center[1]-offset),
                 cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2 )
 
+print( f"After def. area_labels: {area_labels}")
 
+# frame objects have an object id, a center and a 'seen' field.
+last_frame_objects = []  
+object_id=0
+tracking_threshold=10
+def track( contours ):
+    #print( f"contours: {contours}")
+    frame_objects = []
+    global object_id
+    global last_frame_objects
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > area_threshold:
+            frame_object = {} 
+            frame_object["center"]=get_center(contour)
+            frame_object["seen"]=False
+            frame_object["contour"] = contour
+            frame_object["area"] = area
+            for lfo in last_frame_objects:
+                offset_x = frame_object["center"][0] - lfo["center"][0]
+                offset_y = frame_object["center"][1] - lfo["center"][1]
+                if math.hypot(offset_x, offset_y) <= tracking_threshold:
+                    frame_object["object_id"] = lfo["object_id"]
+                    frame_object["seen"] = True
+            #print( f"frame_object: {frame_object}")
+            frame_objects.append(frame_object) 
+    new_frame_objects = []
+    for fo in frame_objects:
+        if fo["seen"] == False:
+            object_id += 1
+            fo["object_id"] = object_id
+        new_frame_objects.append(fo)
+    last_frame_objects = new_frame_objects.copy()
+    #print( object_id )
+    return new_frame_objects
 # Video Read Loop --------------------------------------------------------------
 
 ret, frame = cap.read()
 
 while ret and current_frame <= end_frame:
-    if current_frame >= start_frame:
-        mask = object_detector.apply(frame)
-        _, mask = cv2.threshold( mask, grey_threshold-1, grey_threshold,
-                                 cv2.THRESH_BINARY )
-        contours, _ = cv2.findContours( mask,
-                                        cv2.RETR_TREE,
-                                        cv2.CHAIN_APPROX_SIMPLE )
-        if show_mask:
-            window_label = "Mask"
-            image = mask
-        else:
-            window_label = "Frame"
-            image = frame
-    
-        if grid:
-            show_grid( image )   
+    if current_frame < start_frame:
+        current_frame += 1
+        continue
+    print( f"current_frame: {current_frame}")
+    mask = object_detector.apply(frame)
+    _, mask = cv2.threshold( mask, grey_threshold-1, grey_threshold,
+                                cv2.THRESH_BINARY )
+    contours, _ = cv2.findContours( mask,
+                                    cv2.RETR_TREE,
+                                    cv2.CHAIN_APPROX_SIMPLE )
+    if show_mask:
+        window_label = "Mask"
+        image = mask
+    else:
+        window_label = "Frame"
+        image = frame
 
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            if area > area_threshold:
-                # Add tracking here.
-                current = get_center( image, cnt )
-                centers.append( current )
-                if trails:
-                    cv2.drawContours(image, [cnt], -1, (0, 255, 0), 2)
-                    for center in centers:
-                        red   = 0xFF
-                        color = ( 0, 0, red )
-                        cv2.circle(image, (center[0], center[1]), 2, color, -1)
-                cv2.drawContours(image, [cnt], -1, (0, 255, 0), 2)
-                if area_labels:
-                    area_labels( image, area, 20, 3 )
+    if grid:
+        show_grid( image )   
 
-        out.write(image)
-        cv2.namedWindow(window_label)        # Create a named window
-        cv2.moveWindow(window_label, 40,30)  # Move it to (40,30)
-        cv2.imshow(window_label, image)
 
-        key = cv2.waitKey(1)
-        if key == 27:
-            break
+    tracked_objects=track(contours)
+    #print(f"tracked_objects: {tracked_objects}")
+    for to in tracked_objects:
+        # print(f"to: {to}")
+        centers.append(to["center"])
+        if trails:
+            cv2.drawContours(image, [to["contour"]], -1, (0, 255, 0), 2)
+            for center in centers:
+                red   = 0xFF
+                color = ( 0, 0, red )
+                cv2.circle(image, (center[0], center[1]), 2, color, -1)
+        cv2.drawContours(image, [to["contour"]], -1, (0, 255, 0), 2)
+        # if object_id in to:
+        #     oid=to["object_id"]
+        #     print("to[object_id]: {oid}")
+        #     object_labels( image, to["object_id"], to["center"], 10, 2)
+        oid=to["object_id"]
+        print(f"to[object_id]: {oid} to[center] {to['center']}")
+        object_labels( image, to["object_id"], to["center"], 10, 2)
+        if area_labels:
+            label( image, to["area"], to["center"], 20, 3 )
+
+    out.write(image)
+    cv2.namedWindow(window_label)        # Create a named window
+    cv2.moveWindow(window_label, 40,30)  # Move it to (40,30)
+    cv2.imshow(window_label, image)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
 
     ret, frame = cap.read()
     current_frame += 1
